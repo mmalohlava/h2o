6e64e68874380bf1bad0f99d865e392daad93160
Josh <jreeseue@gmail.com>
2014-03-12 11:11:31 -0400

    Changes made to script for Linux execution.

--- py/testdir_rf/jrun_gen_rf.py
+++ py/testdir_rf/jrun_gen_rf.py
@@ -11,25 +11,24 @@ import h2o_rf
 DATASET_NAME="ebird"
 
 def fG(ds, f): return "/Users/jreese/Google Drive/RF/%s/%s" % (ds, f)
-def f(ds,f): return "/Users/jreese/Documents/uni/purdue/research/dr-api-mock/RF/data/%s/%s" % (DATASET_NAME, f)
+def fP(ds, f): return '/homes/reese5/research/h2o/smalldata/%s/%s' % (ds, f)
+def f(ds,f): return "/Users/jreese/Documents/uni/purdue/research/dr-api-mock/RF/data/%s/%s" % (ds, f)
 
 trees = 10 if len(sys.argv) < 2 else int(sys.argv[1])
     
 ds = DATASET_NAME
 c = r.connect()
 
-# trainKey = c.getHexKey(f(ds, "train_20ki_100c.csv"), parser_type='CSV', separator=',')
-# trainKey = c.getHexKey(f(ds, "train_20ki.csv"), parser_type='CSV', separator=',')
-# trainKey = c.getHexKey(f(ds, "train_100ki.csv"), parser_type='CSV', separator=',')
-trainKey = c.getHexKey(f(ds, "train_1M.csv"), parser_type='CSV', separator=',')
-# trainKey = c.getHexKey(fG(ds, "train.csv"), parser_type='CSV', separator=',')
+# trainKey = c.getHexKey(fP(ds,"train_1K.csv"),parser_type='CSV',separator=',')
+trainKey = c.getHexKey(fP(ds,"train_1M.csv"),parser_type='CSV',separator=',')
+# trainKey = c.getHexKey(f(ds,"train_1M.csv"),parser_type='CSV',separator=',')
+# trainKey = c.getHexKey(fG(ds,"train.csv"),parser_type='CSV',separator=',')
 
 trainResult = c.trainRF(trainKey, ntree=trees,
                         model_key="rf_model_{0}".format(ds))
-
+exit()
 # print "%s" % h2o_rf.pp_rf_result(trainResult)
 
-# testkey = c.getHexKey(f(ds, "test_50c.csv"))
 testkey = c.getHexKey(f(ds, "test_400K.csv"), parser_type='CSV', separator=',')
 # testkey = c.getHexKey(fG(ds, "test.csv"), parser_type='CSV', separator=',')
 testResult = c.scoreRF(testkey, trainResult, out_of_bag_error_estimate=0)
