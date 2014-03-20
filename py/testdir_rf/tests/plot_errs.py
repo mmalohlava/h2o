e3b36ef2ecbd0f7550eab0e4aee4bec0a221cfca
Josh <jreeseue@gmail.com>
2014-03-20 12:59:43 -0400

    CHG: plotting script.

--- py/testdir_rf/tests/plot_errs.py
+++ py/testdir_rf/tests/plot_errs.py
@@ -7,14 +7,24 @@ pdata = []
 for d in data:
     d = d.strip('\n').split(',')
     pdata.append(int(d[0]))
-    pdata.append(float(d[1]))
-    
-print data
-print pdata
-x = pdata[::2]
-y = pdata[1::2]
-plt.plot(x, y)
-# plt.axis([min(x), max(x), min(y), max(y)])
-# show()
-plt.savefig('plot.png')
+    pdata.append(100-float(d[1]))
+    pdata.append(float(d[2]))
 
+print pdata    
+x = pdata[::3]
+y = pdata[1::3]
+z = pdata[2::3]
+print x
+print y
+print z
+plt.plot(x, y,'.-')
+plt.xlabel('# of trees')
+plt.ylabel('% correct')
+plt.axis([0, max(x)+5, min(y)-.2, max(y)+.1])
+# show()
+plt.savefig('err_plot.png')
+plt.clf()
+plt.plot(x,z,'.-')
+plt.ylabel('Time to train (s)')
+plt.xlabel('# of trees')
+plt.savefig('t_plot.png')
